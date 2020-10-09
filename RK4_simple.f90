560058912e5b0528deeb8593d70e755497b5d689
Shang-Yu Chien <s1109119@gmail.com>
2020-10-09 23:59:00 +0800

    Add files via upload
    type: test
    
    Add a example to demonstrate how the RK4 can solve the physical problem.
    
    Example: Damped Driven Oscillator

--- RK4_simple.f90
+++ RK4_simple.f90
@@ -1,7 +1,31 @@
 program testnow
 implicit none
+!----------
+!Example: Damped and driven oscillation
+!F0cos(wt)=m*x''+cx'+kx
+!Consider a mass-spring system.
+real (kind=8) :: m,k,c,F0,J1,J2,J3
+real (kind=8) :: ini_t ,final_t,ini_y,ini_ydot
+m=0.5  !m:mass (unit:kg)
+k=2.0  !spring constant
+c=0.03 !damping term (for friction force)
+F0=1   !Motor can give the periodic force
+!---------------------
+J1=c/m
+J2=k/m
+J3=F0/m !J3*coswt
+
+!print 200, "X''+", J1,"X'+",J2,"X-",J3,"coswt = 0"
+WRITE (*, FMT= 200) "X''+", J1,"X'+",J2,"X-",J3,"coswt = 0"
+200 format(1x, a, 1x,f5.2 , 1x, a, 1x,f5.2 ,1x, a, 1x,f5.2 ,1x,a)
+!100 format (7x,'Name:',f12.3,J3, 7x, 'Id:', 1x, 'Weight:')
+ini_t = 0.d0
+final_t = 20.d0
+ini_y = 0.0d0
+ini_ydot = 0.0d0
 !Input the ini_t,final_t,ini_y and ini_ydot.
-call evaluate(0.d0,1.d0,-0.4d0,-0.6d0) !###Modify###
+!call evaluate(0.d0,1.d0,-0.4d0,-0.6d0) !###Modify###
+call evaluate(ini_t,final_t,ini_y,ini_ydot) !###Modify###
 end
 
 !Define the forms of coefficients.
@@ -9,21 +33,24 @@ end
 real (kind=8) function jae1(t,y)
 implicit none
  real (kind=8) :: t,y
-jae1=-2.d0 !###Modify###
+!jae1=-2.d0 !###Modify###
+jae1=0.06d0 !###Modify###
 return
 end function jae1
 !--------------------------------
 real (kind=8) function jae2(t,y)
 implicit none
 real (kind=8) :: t,y
-jae2=2.d0 !###Modify###
+!jae2=2.d0 !###Modify###
+jae2=4.d0 !###Modify###
 return
 end function jae2
 !--------------------------------
 real (kind=8) function jae3(t,y)
 implicit none
 real (kind=8) :: t,y
-jae3=-DEXP(2*t)*DSIN(t) !###Modify###
+!jae3=-DEXP(2*t)*DSIN(t) !###Modify###
+jae3=-2.d0*DCOS(t)
 return
 end function jae3
 !==================================
@@ -43,7 +70,7 @@ end function f2
 !====================================
 
 subroutine evaluate(ini_t,final_t,ini_y,ini_ydot)
-!Evaluate the differential equation : y''+J1*y'+J2y+J3=0
+!Evaluate the differential equation : y''+J1y'+J2y+J3=0
 !In Runge Kutta 4 ~
 implicit none
 real (kind=8) :: J1,J2,J3 !(Coeficient of differential equation)
@@ -110,8 +137,12 @@ w2=w2+(k(1,2)+2.d0*k(2,2)+2.d0*k(3,2)+k(4,2))/6.d0
 ! print the calulation result
 if(MOD(i,1000000) .eq. 0) then
 !write(*,*)"n=",i
-write(10,*)J1,	J2, J3
-write(9,*)t,	w1,	w2
+!write(10,*)J1,	J2, J3
+WRITE (10, FMT= 109) "t=",t,",J1=", J1,",J2=",J2,",J3=",J3
+109 format(1x, a, 1x,f5.2 , 1x, a, 1x,f5.2 ,1x, a, 1x,f5.2 ,1x,a, 1x,f5.2)
+WRITE (9, FMT= 110) "t=",t,",x=", w1,",v=",w2
+110 format(1x, a, 1x,f5.2 , 1x, a, 1x,f5.2 ,1x, a, 1x,f5.2)
+!write(9,*)t,	w1,	w2
 endif
 
 !The program will stop when w1 is NAN. 
